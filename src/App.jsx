e0b7a7b2233dbbd00c89004aa12db5da6a0e6d1a
Muhammad Umer <umer44447@gmail.com>
2022-06-24 10:49:11 -0700

    Assets and Styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-
+// import 'stream-chat-react/dist/css/index.css';
 import {ChannelListContainer, ChannelContainer} from './components';
+
 import './App.css';
+
 
 const apiKey = 't932vn3q6u7m';
 
@@ -12,7 +14,7 @@ const client =StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div className="app_wrapper">
+    <div className="app__wrapper">
         <Chat client={client} theme="team light" >
             <ChannelListContainer />
             <ChannelContainer />
--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import {ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import {ChannelSearch, TeamChannelSearch, TeamChannelPreview } from './';
+import SlackIcon from '../assets/slack.png';
+import LogoutIcon from '../assets/logout.png';
+
+
+const SideBar= () => {
+    <div style={{ backgroundColor: "pink"}} className="channel-list__sidebar">
+        <div className="channel-list__sidebar_icon1">
+            <div className="icon1__inner">
+                <img src= { SlackIcon } alt="Slack" width="30"/>
+            </div>
+        </div>
+
+        <div className="channel-list__sidebar_icon1">
+            <div className="icon1__inner">
+                <img src= { LogoutIcon } alt="Logout" width="30"/>
+            </div>
+        </div>
+        <img src= { LogoutIcon } alt="Slack" width="30"/>
+
+    </div>
+}
 
 const ChannelListContainer = () => {
   return (
-    <div>
-        Channel List Container
-    </div>
+    <>
+        <SideBar/>
+    </>
   )
 }
 
