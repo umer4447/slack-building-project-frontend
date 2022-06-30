7c28c927d6b111749c368638fa083122ee51deb4
Muhammad Umer <umer44447@gmail.com>
2022-06-30 06:15:49 -0700

    Feature/ Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,59 @@
 import React from 'react';
-import {StreamChat} from 'stream-chat';
-import {Chat} from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-// import 'stream-chat-react/dist/css/index.css';
-import {ChannelListContainer, ChannelContainer, Auth} from './components';
 
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
+
+// import 'stream-chat-react/dist/css/index.css';
 import './App.css';
 
-
+const cookies = new Cookies();
 const apiKey = 't932vn3q6u7m';
 
-const client =StreamChat.getInstance(apiKey);
+const authToken = cookies.get("token");
 
-const authToken = false;
+const client = StreamChat.getInstance(apiKey);
+
+if(authToken) {
+    client.connectUser({
+        id: cookies.get('userId'),
+        name: cookies.get('username'),
+        fullName: cookies.get('fullName'),
+        image: cookies.get('avatarURL'),
+        hashedPassword: cookies.get('hashedPassword'),
+        phoneNumber: cookies.get('phoneNumber'),
+    }, authToken)
+}
+
+
 
 const App = () => {
-  if (!authToken) return <Auth/>
-  return (
-    <div className="app__wrapper">
-        <Chat client={client} theme="team light" >
-            <ChannelListContainer />
-            <ChannelContainer />
-        </Chat>
-        
-    </div>
-  )
+    // const [createType, setCreateType] = useState('');
+    // const [isCreating, setIsCreating] = useState(false);
+    // const [isEditing, setIsEditing] = useState(false);
+
+    if(!authToken) return <Auth />
+
+    return (
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer 
+                    // isCreating={isCreating}
+                    // setIsCreating={setIsCreating}
+                    // setCreateType={setCreateType}
+                    // setIsEditing={setIsEditing}
+                />
+                <ChannelContainer 
+                    // isCreating={isCreating}
+                    // setIsCreating={setIsCreating}
+                    // isEditing={isEditing}
+                    // setIsEditing={setIsEditing}
+                    // createType={createType}
+                />
+            </Chat>
+        </div>
+    );
 }
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,11 @@
 
-export {default as ChannelListContainer} from './ChannelListContainer';
-export {default as ChannelContainer} from './ChannelContainer';
-export {default as ChannelSearch} from './ChannelSearch';
-export {default as TeamChannelList} from './TeamChannelList';
-export {default as TeamChannelPreview} from './TeamChannelPreview';
-export {default as Auth} from './Auth';
+export { default as ChannelContainer } from './ChannelContainer';
+export { default as ChannelListContainer } from './ChannelListContainer';
+export { default as ChannelSearch } from './ChannelSearch';
+export { default as TeamChannelList } from './TeamChannelList';
+export { default as TeamChannelPreview } from './TeamChannelPreview';
+export { default as Auth } from './Auth';
+
+
+
 
